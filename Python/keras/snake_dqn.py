9c5e390289e51611ff7d706d124aabe0001ee107
Dohyun Kim <00dh.kim@gmail.com>
2023-02-26 10:30:34 +0900

    .

--- Python/keras/snake_dqn.py
+++ Python/keras/snake_dqn.py
@@ -6,7 +6,6 @@
 
 from snake_gym import SnakeGym
 
-
 # In[2]:
 
 
@@ -32,6 +31,7 @@ LOAD_MODEL_PATH = './Python/snake_dqn_1.h5'
 
 RENDER = False
 RENDER_DELAY = 0.1
+
 
 class DQNAgent:
     def __init__(self, state_size, action_size):
@@ -63,7 +63,7 @@ class DQNAgent:
 
         if self.load_model:
             self.model.load_weights(LOAD_MODEL_PATH)
-    
+
     # 상태가 입력, 큐함수가 출력인 인공신경망 생성
     def build_model(self):
         model = Sequential()
@@ -76,11 +76,11 @@ class DQNAgent:
         model.summary()
         model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
         return model
-        
+
     # 타깃 모델을 모델의 가중치로 업데이트
     def update_target_model(self):
         self.target_model.set_weights(self.model.get_weights())
-    
+
     # 입실론 탐욕 정책으로 행동 선택
     def get_action(self, state):
         if np.random.rand() <= self.epsilon:
@@ -102,11 +102,11 @@ class DQNAgent:
 
         # 메모리에서 배치 크기만큼 무작위로 샘플 추출
         mini_batch = random.sample(self.memory, self.batch_size)
-        
+
         states = np.zeros((self.batch_size, self.state_size))
         next_states = np.zeros((self.batch_size, self.state_size))
         actions, rewards, dones = [], [], []
-        
+
         for i in range(self.batch_size):
             states[i] = mini_batch[i][0]
             actions.append(mini_batch[i][1])
@@ -126,9 +126,10 @@ class DQNAgent:
             else:
                 target[i][actions[i]] = rewards[i] + self.discount_factor * (
                     np.amax(target_val[i]))
-        
+
         self.model.fit(states, target, batch_size=self.batch_size,
                        epochs=1, verbose=0)
+
 
 # In[5]:
 
@@ -149,80 +150,82 @@ scores, episodes = [], []
 
 save_flag_1, save_flag_2 = True, True
 
+# file create test
+agent.model.save_weights("./snake_dqn_0.h5")
+
 for e in range(EPISODES):
     done = False
     score = 0
-    state = env.reset() # (3, 10, 10)
-    state = np.reshape(state, [1, state_size]) # (1, 300)
-    
+    state = env.reset()  # (3, 10, 10)
+    state = np.reshape(state, [1, state_size])  # (1, 300)
+
     for _ in range(EPISODE_LENGTH):
         if RENDER:
             print(f'episode: {e}')
             env.render(delay=RENDER_DELAY)
-        
+
         # 현재 상태로 행동을 선택
         action = agent.get_action(state)
-        
+
         # 선택한 행동으로 환경에서 한 타임스텝 진행
         next_state, reward, done, info = env.step(action)
-        next_state = np.reshape(next_state, [1, state_size]) # (3, 10, 10) -> (1, 300)
-        
+        next_state = np.reshape(next_state, [1, state_size])  # (3, 10, 10) -> (1, 300)
+
         agent.append_sample(state, action, reward, next_state, done)
-        
+
         # 매 타임스텝마다 학습
         if len(agent.memory) > agent.train_start:
             agent.train_model()
-        
+
         score += reward
         state = next_state
-        
+
         if done:
             break
-    
+
     agent.update_target_model()
     scores.append(score)
     episodes.append(e)
     # pylab.plot(episodes, scores, 'b')
     # pylab.savefig('scores.png')
-    
+
     logging.info(f"episode:{e} score: {score:+} snklen: {info['snake_length']} "
-            f"memlen: {len(agent.memory)} epsilon: {agent.epsilon}")
-    
+                 f"memlen: {len(agent.memory)} epsilon: {agent.epsilon}")
+
     # # 최근 10 에피소드의 점수 평균이 -30 이상일 때 저장
     # if save_flag_1 and np.mean(scores[-min(10, len(scores)):]) > -30:
     #     agent.model.save_weights("./Python/snake_dqn_1.h5")
-    #     logging.info('model 1 saved at {e}')
+    #     logging.info(f'model 1 saved at {e}')
     #     save_flag_1 = False
-    
+
     # # 최근 10 에피소드의 점수 평균이 10 이상일 때 저장
     # if save_flag_2 and np.mean(scores[-min(10, len(scores)):]) > 10:
     #     agent.model.save_weights("./Python/snake_dqn_2.h5")
-    #     logging.info('model 2 saved at {e}')
+    #     logging.info(f'model 2 saved at {e}')
     #     save_flag_2 = False
 
     # # 최근 10 에피소드의 점수 평균이 30 이상일 때 종료
     # if np.mean(scores[-min(10, len(scores)):]) > 30:
     #     agent.model.save_weights("./Python/snake_dqn_3.h5")
-    #     logging.info('model 3 saved at {e}')
+    #     logging.info(f'model 3 saved at {e}')
     #     sys.exit()
-    
+
     # 뱀의 꼬리가 3 이상일 때 저장
     if save_flag_1 and info['snake_length'] > 3:
-        agent.model.save_weights("./Python/snake_dqn_1.h5")
-        logging.info('model 1 saved at {e}')
+        agent.model.save_weights(f"./snake_dqn_1.h5")
+        logging.info(f'model 1 saved at {e}')
         save_flag_1 = False
-    
+
     # 뱀의 꼬리가 5 이상일 때 저장
     if save_flag_2 and info['snake_length'] > 5:
-        agent.model.save_weights("./Python/snake_dqn_2.h5")
-        logging.info('model 2 saved at {e}')
+        agent.model.save_weights("./snake_dqn_2.h5")
+        logging.info(f'model 2 saved at {e}')
         save_flag_2 = False
-    
+
     # 뱀의 꼬리가 10 이상일 때 종료
     if info['snake_length'] > 10:
-        agent.model.save_weights("./Python/snake_dqn_3.h5")
-        logging.info('model 3 saved at {e}')
+        agent.model.save_weights("./snake_dqn_3.h5")
+        logging.info(f'model 3 saved at {e}')
         sys.exit()
-    
-            
+
 # %%
